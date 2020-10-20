a97a84b828bca49e602a125e5e351f8f7ad7c9e9
babyplug <poramin.le.58@ubu.ac.th>
2020-10-20 22:59:27 +0700

    Author -- Add TokenAuthMiddleware to Router

--- src/author/author_controller.go
+++ src/author/author_controller.go
@@ -1,12 +1,15 @@
 package author
 
 import (
+	"github.com/babyplug/go-fiber/src/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func NewAuthorController(router fiber.Router) {
 	SetupAuthorService()
 	authorRouter := router.Group("/authors")
+
+	authorRouter.Use(middleware.TokenAuthMiddleware())
 
 	authorRouter.Get("", handleGet)
 }
